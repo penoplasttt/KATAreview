9b68efad1f924c1c554790c3a89fb9392aa8ce96
penoplasttt <102857538+penoplasttt@users.noreply.github.com>
2023-10-18 19:15:12 +0300

    r5

---
+++ review5/weather/weather.go
@@ -0,0 +1,31 @@
+package weather
+
+import "encoding/json"
+
+
+//go:generate go run github.com/vektra/mockery/v2@v2.20.2 --name=Weather
+type WeatherService interface {
+	GetWeather(data []byte) (Weather, error)
+}
+
+type Weather struct {
+	Temperature string     `json:"temperature"`
+	Wind        string     `json:"wind"`
+	Description string     `json:"description"`
+	Forecast    []Forecast `json:"forecast"`
+}
+
+type Forecast struct {
+	Day         string `json:"day"`
+	Temperature string `json:"temperature"`
+	Wind        string `json:"wind"`
+}
+
+func (w Weather)GetWeather(data []byte) (Weather, error) {
+	var weather Weather
+	err := json.Unmarshal(data, &weather)
+	if err != nil {
+		return weather, err
+	}
+	return weather, err
+}
